b5ddb646db0f5703f2713f9a03118227dc527cab
suraj kumar <sooraj4475@gmail.com>
2025-06-05 23:34:35 +0530

    DAY-5 just coded - freq of element

---
+++ Day5/freqOfelement.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class frequencyofelement {
+	public static void countFreq(int[]arr, int n) {
+		boolean visited[] = new boolean[n];
+	    
+	    Arrays.fill(visited, false);
+
+	    // Traverse through array elements and
+	    // count frequencies
+	    for (int i = 0; i < n; i++) {
+
+	        // Skip this element if already processed
+	        if (visited[i] == true)
+	            continue;
+
+	        // Count frequency
+	        int count = 1;
+	        for (int j = i + 1; j < n; j++) {
+	            if (arr[i] == arr[j]) {
+	                visited[j] = true;
+	                count++;
+	            }
+	        }
+	        System.out.println(arr[i] + " " + count);
+	    }
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int arr[] = new int[]{ 10, 20, 20, 10, 10, 20, 5, 20 };
+		    int n = arr.length;
+		    countFreq(arr, n);
+	}
+
+}
