76bf5ee24d765724a2b461a18dafd2db99f942a4
suraj kumar <sooraj4475@gmail.com>
2025-08-12 18:38:57 +0530

    restarted DSA journey with pattern [prob]

---
+++ Day18/pattern-1.java
@@ -0,0 +1,21 @@
+
+public class reverseeee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         //pattern prob -1 
+		// ****
+		// ****
+		// ****
+		// ****
+		
+		for(int i=0;i<4;i++) {
+			for(int j=0;j<4;j++) {
+				System.out.print("*");
+
+			}
+			System.out.println();
+		}
+	}
+
+}
